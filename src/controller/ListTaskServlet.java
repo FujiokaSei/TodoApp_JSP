5913dea7bfd4d6196d3219452c95899b804ae3db
FujiokaSei <f.sei3123@gmail.com>
2021-12-06 14:54:31 +0900

    ListTaskServlet.java, listTask.jspを追加

---
+++ src/controller/ListTaskServlet.java
@@ -0,0 +1,49 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.DaoFactory;
+import dao.TaskDao;
+import domain.Task;
+
+/**
+ * Servlet implementation class ListTaskServlet
+ */
+@WebServlet("/listTask")
+public class ListTaskServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/view/listTask.jsp")
+				.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			TaskDao taskDao = DaoFactory.createTaskDao();
+			List<Task> taskList = taskDao.findAll();
+			request.setAttribute("taskList", taskList);
+			request.getRequestDispatcher("/WEB-INF/view/listTask.jsp")
+					.forward(request, response);
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}
+		//request.setAttribute(getServletName(), response);
+	}
+
+}
