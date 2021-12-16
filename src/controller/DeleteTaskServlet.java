5fee38efa790fbcb9b3f8c1ee07551df3e6864b5
Fujioka Sei <f.sei3123@gmail.com>
2021-12-16 23:21:01 +0900

    DeleteTaskServlet.javaを作成

---
+++ src/controller/DeleteTaskServlet.java
@@ -0,0 +1,47 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.DaoFactory;
+import dao.TaskDao;
+import domain.Task;
+
+/**
+ * Servlet implementation class DeleteTaskServlet
+ */
+@WebServlet("/deleteTask")
+public class DeleteTaskServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		try {
+			String strId = request.getParameter("id");
+			Task task = new Task();
+			task.setId(Integer.parseInt(strId));
+			TaskDao taskDao = DaoFactory.createTaskDao();
+			TaskDao.delete(task);
+			request.getRequestDispatcher("/WEB-INF/view/main.jsp")
+					.forward(request, response);
+
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
